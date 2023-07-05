080a28dcef141ed8a36daf7729c4f9a890dbe050
Ayaa05 <aya18abbadi@gmail.com>
2023-07-05 13:44:13 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -27,7 +27,10 @@ int check_prime(int n, int i)
 		return (0);
 
 	if (n % i == 0 && i > 1)
+		return (0);
+	
+	if ((n / i) < i)
 		return (1);
 
-	return (checl_prime(n, i + 1));
+	return (check_prime(n, i + 1));
 }
