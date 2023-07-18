115f5336afaab6617e15ac9880332051493961da
Ayaa05 <aya18abbadi@gmail.com>
2023-07-18 14:25:00 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,19 +1,27 @@
-#ifndef _DOG_H_
-#define _DOG_H_
+#ifndef DOG_H
+#define DOG_H
 
 /**
- * struct dog - a dog struct
- * @name: dog's name
- * @age: dog's age
- * @owner: owner's name
+ * struct dog - Structure representing formation about the dog
+ * @name; the name of the dog
+ * @age: Age of the dog
+ * @owner: the name of the owner
  *
- * Description: just a lone dog struct in a big kitty world
+ * Description: This structure stores in formation about a dog
+ * it's name, age and owner "s name".
  */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name,float age, char *owner) ;
+void print_dog(struct dog *d) ;
+typedef struct dog dog_t;
+dog_t *new_dog (char *name, float age,char *owner);
+void free_dog (dog_t *d) ;
 
 #endif
