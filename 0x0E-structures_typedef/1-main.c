b15b207920e8b126d3b76eac01222d22c6fdce7b
Ayaa05 <aya18abbadi@gmail.com>
2023-08-16 06:53:30 -0700

    a

--- 0x0E-structures_typedef/1-main.c
+++ 0x0E-structures_typedef/1-main.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+@#include <stdio.h>
 #include "dog.h"
 /**
  * main - check the code
@@ -6,9 +6,8 @@
  * Return: Always 0.
  */
 int main(void)
-{
-	struct dog my_dog;
-
+{struct dog my_dog;
+	
 	init_dog(&my_dog, "Poppy", 3.5, "Bob");
 	printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
 	return (0);
