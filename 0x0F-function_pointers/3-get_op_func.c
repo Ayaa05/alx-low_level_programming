d50f8a86a3543fe4ca039606a0161915fd976138
Ayaa05 <aya18abbadi@gmail.com>
2023-08-17 08:38:50 -0700

    a

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,7 +1,7 @@
 #include "3-calc.h"
 /**
  * get_op_func -  selects the correct function to perform
- * 			the operation asked by the user
+ * the operation asked by the user
  * @s: operator that used
  * Return: function that make operator or NULL if fails.
  */
