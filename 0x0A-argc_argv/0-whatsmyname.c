74a5deace6db2b549e57c912d35b7bec0c19a7df
Ayaa05 <aya18abbadi@gmail.com>
2023-08-22 13:12:48 -0700

    a

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
-#include "main.h"
-
 /**
- * main - prints the name of the program
- * @argc: number of arguments
- * @argv: array of arguments
+ * main - prints its name, followed by a new line.
  *
- * Return: Always 0 (Success)
+ * @argc: number of command line arguments.
+ *
+ * @argv: array that contains the program command line arguments.
+ *
+ * Return: 0
  */
-
 int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s\n", *argv);
+	printf("%s\n", argv[0]);
 	return (0);
 }
--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
-#include "main.h"
-
 /**
- * main - print the number of arguments passed to the program
- * @argc: number of arguments
- * @argv: array of arguments
+ * main - prints the number of arguments passed into it.
  *
- * Return: Always 0 (Success)
+ * @argc: number of command line arguments.
+ *
+ * @argv: array that contains the program command line arguments.
+ *
+ * Return: 0
  */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-(void) argv; /*Ignore argv*/
-printf("%d\n", argc - 1);
-
-return (0);
+	printf("%d\n", argc - 1);
+	return (0);
 }
